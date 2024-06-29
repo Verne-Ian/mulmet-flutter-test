e489ee42b3b1bbba70dd632568534cf7c20f6784
Lubinga Ian <ianandy778@gmail.com>
2024-06-29 21:17:56 +0300

    Added navigation bar.

--- lib/interface/home.dart
+++ lib/interface/home.dart
@@ -51,30 +51,33 @@ class _MyHomePageState extends State<MyHomePage> {
         centerTitle: true,
       ),
       drawer: const NavBar(),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'Welcome to my App',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+      body: Padding(
+        padding: const EdgeInsets.only(left: 8.0, right: 8.0),
+        child: Center(
+          // Center is a layout widget. It takes a single child and positions it
+          // in the middle of the parent.
+          child: Column(
+            // Column is also a layout widget. It takes a list of children and
+            // arranges them vertically. By default, it sizes itself to fit its
+            // children horizontally, and tries to be as tall as its parent.
+            //
+            // Column has various properties to control how it sizes itself and
+            // how it positions its children. Here we use mainAxisAlignment to
+            // center the children vertically; the main axis here is the vertical
+            // axis because Columns are vertical (the cross axis would be
+            // horizontal).
+            //
+            // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
+            // action in the IDE, or press "p" in the console), to see the
+            // wireframe for each widget.
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                'Welcome to my App',
+                style: Theme.of(context).textTheme.headlineMedium,
+              ),
+            ],
+          ),
         ),
       ),
       floatingActionButton: FloatingActionButton(
--- lib/widgets/navbar.dart
+++ lib/widgets/navbar.dart
@@ -1,10 +1,88 @@
 import 'package:flutter/material.dart';
+import 'package:mulmet_flutter_test/interface/firebase_crud.dart';
 
 class NavBar extends StatelessWidget {
   const NavBar({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    double w = MediaQuery.of(context).size.width;
+
+    return Container(
+      width: w * 0.7,
+      constraints: const BoxConstraints(maxWidth: 480),
+      child: Scaffold(
+        body: SafeArea(
+          child: Drawer(
+            width: w * 0.7,
+            child: ListView(
+              padding: EdgeInsets.zero,
+              children: [
+                ListTile(
+                  tileColor: Colors.orangeAccent.shade100,
+                  selectedTileColor: Colors.orangeAccent,
+                  title: const Text('Firebase Create Sample'),
+                  leading: const Icon(Icons.create, color: Colors.green,),
+                  onTap: (){
+                    Navigator.push(context, MaterialPageRoute(builder: (context)=>const FirebaseCrud(opType: 'create')));
+                  },
+                ),
+                ListTile(
+                  tileColor: Colors.orangeAccent.shade100,
+                  selectedTileColor: Colors.orangeAccent,
+                  title: const Text('Firebase Read Sample'),
+                  leading: const Icon(Icons.read_more, color: Colors.blue,),
+                    onTap: (){
+                      Navigator.push(context, MaterialPageRoute(builder: (context)=>const FirebaseCrud(opType: 'read')));
+                    }
+                ),
+                ListTile(
+                  tileColor: Colors.orangeAccent.shade100,
+                  selectedTileColor: Colors.orangeAccent,
+                  title: const Text('Firebase Update Sample'),
+                  leading: const Icon(Icons.update, color: Colors.yellow,),
+                    onTap: (){
+                      Navigator.push(context, MaterialPageRoute(builder: (context)=>const FirebaseCrud(opType: 'update')));
+                    }
+                ),
+                ListTile(
+                  tileColor: Colors.orangeAccent.shade100,
+                  selectedTileColor: Colors.orangeAccent,
+                  title: const Text('Firebase Delete Sample'),
+                  leading: const Icon(Icons.delete_forever, color: Colors.red,),
+                    onTap: (){
+                      Navigator.push(context, MaterialPageRoute(builder: (context)=>const FirebaseCrud(opType: 'delete')));
+                    }
+                ),
+              ],
+            ),
+          ),
+        ),
+        bottomSheet: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Row(
+                mainAxisSize: MainAxisSize.max,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Expanded(child: ElevatedButton(onPressed: (){}, child: const Text("Sign Out")))
+                ],
+              ),
+              const SizedBox(height: 10.0,),
+              Row(
+                mainAxisSize: MainAxisSize.max,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Expanded(child: ElevatedButton(onPressed: (){}, child: const Text("Exit App")))
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
